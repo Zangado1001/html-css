d63073a8278ccb55bd8131425650e9269632fbad
Gustavo Silva <adm.gustavobezerra@gmail.com>
2024-09-09 22:59:07 -0300

    adicionei site teste imersão alura

---
+++ imersao/dados.js
@@ -0,0 +1,26 @@
+let dados = [ 
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens.",
+        wiki: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        instagram: "https://www.instagram.com/rayssalealsk8/?hl=pt-br",
+        tags: "skate fada sk8"
+    } ,
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma das maiores ginastas brasileiras de todos os tempos. Com sua força, elegância e determinação, ela conquistou o coração dos brasileiros e se tornou um verdadeiro ícone do esporte nacional.",
+        wiki: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        instagram: "https://www.instagram.com/rebecarandrade/?hl=pt-br",
+        tags: "ginástica ginastica ouro"
+
+    } ,
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos, trazendo orgulho para o país. Sua força e técnica a colocaram entre as melhores do mundo.",
+        wiki: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        instagram: "https://www.instagram.com/beatrizsouzaar/?hl=pt-br",
+        tags: "judô judo judoca judoka bia"
+    }
+];
+
+ 
